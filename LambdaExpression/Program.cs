650c64e323dab0c74b75266d5f1b4f421840992e
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-30 23:09:31 +0530

    Person Data Managment is done

--- LambdaExpression/Program.cs
+++ LambdaExpression/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Lembda Expression");
+            List<Person> listPersonInCity = new List<Person>();
+            LambdaDemo.AddRecords(listPersonInCity);
         }
     }
 }
